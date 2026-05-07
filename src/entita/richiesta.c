83e7234ab91d276b91112266b6d577a6f76c768b
Raffaele Pio Villani <raffaelevillani95@gmail.com>
2026-05-07 09:51:56 +0200

    [SETUP] Fix percorsi #include per risolvere gli errori di compilazione

--- src/entita/richiesta.c
+++ src/entita/richiesta.c
@@ -1,5 +1,5 @@
 #include "entita/richiesta.h"
-#include "utilita.h"
+#include "main/utilita.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
--- src/entita/tecnico.c
+++ src/entita/tecnico.c
@@ -1,5 +1,5 @@
 #include "../../include/entita/tecnico.h"
-#include "utilita.h"
+#include "main/utilita.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
